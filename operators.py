f53d09dfbb90a5245639d292bba8111617bafa6d
JayashreeM97 <153632282+JayashreeM97@users.noreply.github.com>
2024-01-16 15:26:52 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,19 @@
+#Arthemetic Operators +,-,/,*,%
+a=67
+b=57
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)
+#comparision operation >,<,==
+print(a>b)
+print(a<b)
+print(a==b)
+print(a<=b)
+print(a>=b)
+#logical operators and,or,not
+print(a<100 and b<a)
+print(a<b or b<a)
+print(not(a>b and b<a))#returns False if the result is true
+ 
---
+++ typeCasting.py
@@ -0,0 +1,18 @@
+#int()
+j='123490'
+i=int(j)
+print(i) #output 123490
+
+#float()
+a=float(j)
+print(a) #output 123490.0
+
+#string
+c=str(i)
+print(c)#output 123490
+
+#boolean
+d=bool(j)
+print(d)#output True
+
+print(type(j))
