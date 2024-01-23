c0bc0637319c6f1f697a2fa53a553b34bd3a2a18
JayashreeM97 <153632282+JayashreeM97@users.noreply.github.com>
2024-01-23 17:42:25 +0530

    Add files via upload

---
+++ Whileloop Assignment.py
@@ -0,0 +1,8 @@
+name=""
+password=""
+while name != "aman" or password !=889900:
+    name= input("enter your name : ")
+    password=int(input("Enter your password:"))
+print("hello aman thank you for verification")
+
+
